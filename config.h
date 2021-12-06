cf2fdd9e6c1ed1787ec59a16b17305478c769ff7
Jesse <jessebot.noreply@protonmail.com>
2021-12-05 18:59:20 -0800

    update config

--- config.h
+++ config.h
@@ -95,7 +95,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static char *dmenucmd[] = { "dmenu_run", "-p", "Launch program:", "-b", "-h", "30", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static char *dmenucmd[] = { "dmenu_run", "-p", "(W-p) Launch program:", "-b", "-h", "30", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 /*
